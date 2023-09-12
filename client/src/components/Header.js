e56c86c7287db0b6e8d2c8221f68ba6fbd8a5186
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-09-11 22:21:15 -0400

    client interface done, working on redux toolkit store

---
+++ client/src/components/Header.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import {Box,AppBar,Toolbar,Button, Typography,Tabs,Tab} from '@mui/material'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    const [value,setValue] = useState(0)
+    return (
+        <>
+        <AppBar position="sticky">
+            <Toolbar>
+                <Typography variant="h4">
+                    My Blog App
+                </Typography>
+                <Box display={'flex'} marginLeft={"auto"} marginRight={"auto"}>
+                    <Tabs textColor='inherit' value={value} onChange={(e,val)=>setValue(val)}>
+                        <Tab label="Blogs" LinkComponent={Link} to="/blogs"/>
+                        <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs"/>
+                    </Tabs>
+                </Box>
+                <Box display={"flex"} marginLeft={"auto"}>
+                    <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to="/login">Login</Button>
+                    <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to="/register">Register</Button>
+                    <Button sx={{margin:1,color:"white"}}>Logout</Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+        </>
+    )
+}
+
+export default Header
---
+++ client/src/redux/store.js
@@ -0,0 +1,13 @@
+import {createSlice, configureStore} from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        isLogin:false 
+    },
+    reducers:{
+        login(state){
+            state.isLogin=true
+        }
+    }
+})
