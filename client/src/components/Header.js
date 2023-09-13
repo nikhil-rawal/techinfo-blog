95cc7a157164316c34b3a67c0bad2b0d80a58ad7
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-09-12 23:59:40 -0400

    redux in logout working successfully

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,26 @@
 import React,{useState} from 'react'
 import {Box,AppBar,Toolbar,Button, Typography,Tabs,Tab} from '@mui/material'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector,useDispatch } from 'react-redux'
+import { authActions } from '../redux/store'
 
 const Header = () => {
     //global state
     const isLogin=useSelector(state=>state.isLogin)
-
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     //use state
     const [value,setValue] = useState(0)
+
+    const handleLogout = () => {
+        try {
+            dispatch(authActions.logout())
+            alert('User Logged Out')
+            navigate("/login")
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <>
         <AppBar position="sticky">
@@ -25,7 +37,7 @@ const Header = () => {
                 <Box display={"flex"} marginLeft={"auto"}>
                     {!isLogin && (<><Button sx={{margin:1,color:"white"}} LinkComponent={Link} to="/login">Login</Button>
                     <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to="/register">Register</Button></>)}
-                    {isLogin && (<><Button sx={{margin:1,color:"white"}}>Logout</Button></>)}
+                    {isLogin && (<><Button onClick={handleLogout} sx={{margin:1,color:"white"}}>Logout</Button></>)}
                 </Box>
             </Toolbar>
         </AppBar>
